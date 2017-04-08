a645c6ed3df53fb7fc0b2a1b2b9d6c3c00e4b970
Mike Zrimsek <mikezrimsek@gmail.com>
2017-04-08 19:48:16 -0400

    add config files

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require("path");
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname, "src/index.js")
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: "source-map",
+  module: {
+    loaders: [{
+      test: /\.js/,
+      include: [path.resolve(__dirname, "src")],
+      loader: "babel"
+    }]
+  },
+  resolve: {
+    root: path.resolve(__dirname, "src")
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM",
+  }
+};
